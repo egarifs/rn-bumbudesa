f93d037549aa0e1629c295e1bd0ae341c62769b4
firdauscode <egarifs25@gmail.com>
2021-06-19 06:54:28 +0700

    Welcoming User

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,46 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Image, StyleSheet, Text, View} from 'react-native';
+import {ProfileDummy} from '../../assets';
 
 const Home = () => {
   return (
     <View>
-      <Text>Home Page Screen</Text>
+      <View style={styles.profileContainer}>
+        <View>
+          <Text style={styles.appName}>Food Market</Text>
+          <Text style={styles.desc}>Let's get some foods</Text>
+        </View>
+        <Image source={ProfileDummy} style={styles.profile} />
+      </View>
+      <Text>Home Page</Text>
     </View>
   );
 };
 
 export default Home;
+
+const styles = StyleSheet.create({
+  profileContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 24,
+    paddingTop: 30,
+    paddingBottom: 24,
+    backgroundColor: 'white',
+  },
+  appName: {
+    fontSize: 22,
+    fontFamily: 'Poppins-Medium',
+    color: '#020202',
+  },
+  desc: {
+    fontSize: 14,
+    fontFamily: 'Poppins-Light',
+    color: '#8D92A3',
+  },
+  profile: {
+    width: 50,
+    height: 50,
+    borderRadius: 8,
+  },
+});
